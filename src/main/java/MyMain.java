9d9b417c08e1913ea6bbec611c3463a7d560c87f
dorian <dekolis1218@gmail.com>
2020-09-12 16:51:20 -0700

    Create babylonian, factorial, and calculateE methods

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,33 +4,44 @@ public class MyMain {
     
     // Calculates the square root iteratively, using the Babylonian method
     public static double babylonian(double x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double n = 10;
+        double m = 100;
+        while (Math.abs(n - m) >= 0.00001) {
+            m = n;
+            n = (n + x/n) / 2;
+        }
+        return n;
     }
 
     // Calculates the factorial of a number
     public static double factorial(int x) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        for (int i = x-1; i > 0; i--) {
+            x*=i;
+        }
+        return (x == 0) ? 1 : x;
     }
 
     // Calculates the value for the math constant e iteratively
     // goes until it calculates an answer less than 0.00000000001 
     // different from the value Math.E
+    // e = 1/factorial(0) + 1/factorial(1) + 1/factorial(2) + 1/factorial(3) + ... + 1/factorial(infinity).
     public static double calculateE() {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        double e = 0;
+        int i = 0;
+        while (Math.E - e > 0.00000000001) {
+            e += (1/factorial(i));
+            i++;
+        }
+        return e;
     }
     
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        // YOUR CODE HERE
-
-
-        
-
+        System.out.println("Enter a number to find the square root of: ");
+        double number = scan.nextDouble();
+        System.out.println("The square root of " + number + " is " + babylonian(number));
+        System.out.println("The value of e is roughly : " + calculateE());
         scan.close();
     }
 }
